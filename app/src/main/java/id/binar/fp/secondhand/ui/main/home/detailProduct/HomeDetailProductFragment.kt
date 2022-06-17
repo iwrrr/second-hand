a8a9cdd85d669af41ab7205b7c0ff29814231b0f
Galih0123 <garakasw220@gmail.com>
2022-06-17 20:14:04 +0700

    SP-1.3

---
+++ app/src/main/java/id/binar/fp/secondhand/ui/main/home/detailProduct/HomeDetailProductFragment.kt
@@ -0,0 +1,35 @@
+package id.binar.fp.secondhand.ui.main.home.detailProduct
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import id.binar.fp.secondhand.R
+import id.binar.fp.secondhand.databinding.FragmentHomeDetailProductBinding
+
+
+class HomeDetailProductFragment : Fragment() {
+    private var _binding: FragmentHomeDetailProductBinding? = null
+    private val binding get() = _binding!!
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentHomeDetailProductBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+}
