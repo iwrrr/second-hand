742ea5110ff1af4ab5741e9fa7b013813a2dbd6c
Irya Muhammad <iryadev15@gmail.com>
2022-06-20 12:07:01 +0700

    finish ui bidder info

--- app/src/main/java/id/binar/fp/secondhand/ui/main/adapter/sell/SellListPagerAdapter.kt
+++ app/src/main/java/id/binar/fp/secondhand/ui/main/adapter/sell/SellListPagerAdapter.kt
@@ -4,9 +4,9 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import id.binar.fp.secondhand.ui.main.sell.interested.SellListInterestedFragment
-import id.binar.fp.secondhand.ui.main.sell.product.SellListProductFragment
-import id.binar.fp.secondhand.ui.main.sell.sold.SellListSoldFragment
+import id.binar.fp.secondhand.ui.main.sell.interested.InterestedFragment
+import id.binar.fp.secondhand.ui.main.sell.product.ProductFragment
+import id.binar.fp.secondhand.ui.main.sell.sold.SoldFragment
 
 class SellListPagerAdapter(fm: FragmentManager, lifecycle: Lifecycle) :
     FragmentStateAdapter(fm, lifecycle) {
@@ -16,9 +16,9 @@ class SellListPagerAdapter(fm: FragmentManager, lifecycle: Lifecycle) :
     override fun createFragment(position: Int): Fragment {
         var fragment: Fragment? = null
         when (position) {
-            0 -> fragment = SellListProductFragment()
-            1 -> fragment = SellListInterestedFragment()
-            2 -> fragment = SellListSoldFragment()
+            0 -> fragment = ProductFragment()
+            1 -> fragment = InterestedFragment()
+            2 -> fragment = SoldFragment()
         }
         return fragment as Fragment
     }
